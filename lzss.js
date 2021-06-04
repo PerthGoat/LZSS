4a8cdec7c289eb4d168c560eb019de67c9a5a2d7
TwixKitty <rybothecamel@gmail.com>
2021-06-04 03:01:55 -0400

    rewrote lzss code to make it better

---
+++ lzss.js
@@ -0,0 +1,101 @@
+const sliding_window_sz = 256;
+
+function compressText(str) {
+  // compress
+  let sw = '';
+  let match = '';
+  let strout = '';
+  
+  for(let i = 0;i < str.length;i++) {
+    if(sw.includes(match + str[i])) {
+      match += str[i];
+      continue;
+    }
+    
+    if(match.length >= 5) {
+      let len = match.length;
+      let pos = sw.indexOf(match);
+      strout += `[${pos.toString(36)},${len.toString(36)}]`;
+    } else {
+      strout += match;
+    }
+    sw += match;
+    while(sw.length > sliding_window_sz) {
+      sw = sw.substr(1);
+    }
+    match = str[i];
+  }
+
+  if(match.length >= 5) {
+    let len = match.length;
+    let pos = sw.indexOf(match);
+    strout += `[${pos.toString(36)},${len.toString(36)}]`;
+  } else {
+    strout += match;
+  }
+  
+  return strout;
+}
+
+function decompressText(str) {
+  // decompress
+  sw = '';
+  let dcmpr = '';
+
+  for(let i = 0;i < str.length;i++) {
+    if(str[i] == '[') {
+      let vals = '';
+      i++;
+      while(str[i] != ']' && i < str.length) {
+        vals += str[i];
+        i++;
+      }
+      let splt = vals.split(',');
+      let pos = parseInt(splt[0], 36);
+      let len = parseInt(splt[1], 36);
+      
+      let str_chunk = sw.substr(pos, len);
+      
+      sw += str_chunk;
+      while(sw.length > sliding_window_sz) {
+        sw = sw.substr(1);
+      }
+      
+      dcmpr += str_chunk;
+    } else {
+      sw += str[i];
+      while(sw.length > sliding_window_sz) {
+        sw = sw.substr(1);
+      }
+      dcmpr += str[i];
+    }
+  }
+  
+  return dcmpr;
+}
+
+function compressButton() { 
+  let the_text = document.getElementById('in').value;
+  let comp_text = compressText(the_text);
+  document.getElementById('comp').value = comp_text;
+  
+  let ratio = comp_text.length / the_text.length;
+  
+  document.getElementById('ratio').innerText = `compression ratio: ${ratio.toFixed(3) * 100}%`;
+}
+
+function decompressButton() {
+  let the_text = document.getElementById('comp').value;
+  document.getElementById('decomp').value = decompressText(the_text);
+}
+
+
+
+//console.assert(dcmpr == d, 'decompression from the compressed string failed');
+
+//console.log(dcmpr);
+
+//console.log(`compression ratio: ${strout.length / d.length}`);
+
+//sw += match;
+//console.log(sw);
